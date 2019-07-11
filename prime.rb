96b7ec1bf21a4b9466596b185ee8eb9bb3823f74
Celal Cankaya <celalcankaya@outlook.com>
2019-07-11 12:14:52 +0000

    Done.

--- prime.rb
+++ prime.rb
@@ -1,1 +1,14 @@
+
 # Add  code here!
+def prime?(number)
+  n = 2
+  while n < number
+    return false if number % n == 0
+    n += 1
+  end
+  if number < 2
+    false
+  else
+    true
+  end
+end
